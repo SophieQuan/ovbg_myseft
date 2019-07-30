6d03a9a31701f2619e29bbdf3cb0ecd23c54933f
SophieQuan <51676699+SophieQuan@users.noreply.github.com>
2019-07-30 16:28:17 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -756,39 +756,39 @@ window.onload = function () {
 	// END IMAGE GALLERY SCRIPT ----------
 
 	/*ANIMATE SLASH SCREEN*/   
-/*ANIMATE SLASH SCREEN*/   
-    $("#splash").show();
+	/*ANIMATE SLASH SCREEN*/   
+	    /*$("#splash").show();
 
-    TweenMax.from("#splash", 0.5, {
-        delay: 0.25,
-        opacity: 0,
-        ease: Sine.easeIn,
+	    TweenMax.from("#splash", 0.5, {
+		delay: 0.25,
+		opacity: 0,
+		ease: Sine.easeIn,
 
-    });
+	    });
 
-    TweenMax.fromTo("#slashLogo",1,{
-        delay: 0.5,
-        scale: 0.8,
-        ease: Sine.easeIn,
-        opacity: 0, 
-    },{ 
-        delay: 0.5,
-        scale: 1,
-        ease: Sine.easeOut,
-        opacity: 1,
-        
-    });
+	    TweenMax.fromTo("#slashLogo",1,{
+		delay: 0.5,
+		scale: 0.8,
+		ease: Sine.easeIn,
+		opacity: 0, 
+	    },{ 
+		delay: 0.5,
+		scale: 1,
+		ease: Sine.easeOut,
+		opacity: 1,
 
-    TweenMax.to("#splash", 0.25, {
-        delay: 3,
-        opacity: 0,
-        ease: Sine.easeOut,
-        onComplete: loadMap
-    });
-    
-    /*function show map screen on complete animate the logo*/
-    function loadMap()
-        {
-            $("#map_app").show();
-        };
+	    });
+
+	    TweenMax.to("#splash", 0.25, {
+		delay: 3,
+		opacity: 0,
+		ease: Sine.easeOut,
+		onComplete: loadMap
+	    });*/
+
+	    /*function show map screen on complete animate the logo*/
+	    /*function loadMap()
+		{
+		    $("#map_app").show();
+		};*/
 };
